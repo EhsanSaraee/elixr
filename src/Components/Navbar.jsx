1898dc39fdbcab2d5d46b5b0c758a962a3b9c346
ehsan <ehsansaraee6570@yahoo.com>
2022-02-17 11:57:50 +0330

    navbar component develop & links added

--- src/Components/Navbar.jsx
+++ src/Components/Navbar.jsx
@@ -1,9 +1,35 @@
+import { Link } from 'react-router-dom';
+import styled from 'styled-components';
+import { menuLinks } from '../Data/menuLinks';
+
 const Navbar = () => {
    return (
-      <>
-         <h1>Navbar</h1>
-      </>
+      <Nav>
+         <Logo to="/">ELIXR</Logo>
+         <MenuBars />
+         <NavMenu>
+            {menuLinks?.map((item, index) => (
+               <NavMenuLink key={index} to={item.link}>
+                  {item.title}
+               </NavMenuLink>
+            ))}
+         </NavMenu>
+      </Nav>
    );
 };
+
+const Nav = styled.nav`
+   
+`;
+
+const Logo = styled(Link)`
+   
+`;
+
+const MenuBars = styled.i``;
+
+const NavMenu = styled.div``;
+
+const NavMenuLink = styled(Link)``;
 
 export default Navbar;
---
+++ src/Data/menuLinks.js
@@ -0,0 +1,5 @@
+export const menuLinks = [
+   { title: 'About', link: '/about' },
+   { title: 'Houses', link: '/houses' },
+   { title: 'Rentals', link: '/rentals' },
+];
