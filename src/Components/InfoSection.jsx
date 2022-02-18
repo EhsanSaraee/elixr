45ae71e0b187d475473e2174b068ef4530951ba5
ehsan <ehsansaraee6570@yahoo.com>
2022-02-18 20:50:22 +0330

    infosection styles added & finish

--- src/Components/InfoSection.jsx
+++ src/Components/InfoSection.jsx
@@ -16,7 +16,9 @@ const InfoSection = ({
                <h1>{heading}</h1>
                <p>{paragraphOne}</p>
                <p>{paragraphTwo}</p>
-               <Button to="/houses">{buttonLabel}</Button>
+               <Button to="/houses" primary="true">
+                  {buttonLabel}
+               </Button>
             </ColumnLeft>
             <ColumnRight reverse={reverse}>
                <img src={image} alt="house" />
@@ -26,12 +28,63 @@ const InfoSection = ({
    );
 };
 
-const Section = styled.section``;
+const Section = styled.section`
+   width: 100%;
+   height: 100%;
+   padding: 4rem 0rem;
+`;
 
-const Container = styled.div``;
+const Container = styled.div`
+   padding: 3rem calc((100vw - 1300px) / 2);
+   display: grid;
+   grid-template-columns: 1fr 1fr;
+   grid-template-rows: 800px;
 
-const ColumnLeft = styled.div``;
+   @media screen and (max-width: 768px) {
+      grid-template-columns: 1fr;
+   }
+`;
 
-const ColumnRight = styled.div``;
+const ColumnLeft = styled.div`
+   display: flex;
+   flex-direction: column;
+   justify-content: center;
+   align-items: flex-start;
+   line-height: 1.4;
+   padding: 1rem 2rem;
+   order: ${({ reverse }) => (reverse ? '2' : '1')};
+
+   h1 {
+      margin-bottom: 1rem;
+      font-size: clamp(1.5rem, 6vw, 2rem);
+   }
+
+   p {
+      margin-bottom: 2rem;
+   }
+`;
+
+const ColumnRight = styled.div`
+   padding: 1rem 2rem;
+   order: ${({ reverse }) => (reverse ? '1' : '2')};
+   display: flex;
+   align-items: center;
+   justify-content: center;
+
+   @media screen and (max-width: 768px) {
+      order: ${({ reverse }) => (reverse ? '2' : '1')};
+   }
+
+   img {
+      width: 100%;
+      height: 100%;
+      object-fit: cover;
+
+      @media screen and (max-width: 768px) {
+         width: 90%;
+         height: 90%;
+      }
+   }
+`;
 
 export default InfoSection;
